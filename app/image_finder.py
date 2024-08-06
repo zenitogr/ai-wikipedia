4e1432acfc93c4da5cfcf84987f3f7d8001e59bc
ZenitoGR <zengodgr@outlook.com>
2024-08-07 01:40:04 +0300

    try 2

--- app/image_finder.py
+++ app/image_finder.py
@@ -1,18 +1,22 @@
 import requests
-from config import Config
 
 def get_image_url(query):
-    url = f"https://api.unsplash.com/search/photos"
+    url = "https://en.wikipedia.org/w/api.php"
     params = {
-        "query": query,
-        "per_page": 1,
-        "client_id": Config.UNSPLASH_ACCESS_KEY
+        "action": "query",
+        "format": "json",
+        "prop": "pageimages",
+        "piprop": "original",
+        "titles": query,
+        "pithumbsize": 300
     }
     response = requests.get(url, params=params)
     data = response.json()
     
-    if data['results']:
-        return data['results'][0]['urls']['small']
+    pages = data['query']['pages']
+    for page in pages.values():
+        if 'original' in page:
+            return page['original']['source']
     return None
 
 def get_images_for_suggestions(image_suggestions):
